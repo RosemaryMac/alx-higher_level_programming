f4f5955eea215e37a81234fe971f97ac86ce9bb0
RosemaryMac <rmachanja14@gmail.com>
2022-08-25 05:04:33 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add(a, b):
-    a = 1
-    b = 2
-
-    for args in add:
-        args[i] = a[i] + b[i]
-        print(args)
+    if __name__ == "__main__":
+        from add_0 import add
+        a = 1
+        b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    return (a + b)
