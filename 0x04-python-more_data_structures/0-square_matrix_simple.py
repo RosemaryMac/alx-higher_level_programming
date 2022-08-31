35116b966c821ffc1f0b85c3e8f5a112c26a0ac9
RosemaryMac <rmachanja14@gmail.com>
2022-08-31 21:10:33 +0300

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix = [
-            [2, 3, 4],
-            [5, 6, 7],
-            ]
-
-    for x in matrix[]:
-        print(x)
+    return [list(join((lambda x: x * x), elm)) for elm in matrix]
