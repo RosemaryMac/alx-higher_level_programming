6df6f042faff57bf07b0b48ba9243e7aa7c03154
RosemaryMac <rmachanja14@gmail.com>
2023-01-18 20:37:17 +0300

    web scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,10 @@
 const request = require('request');
 const fpath = PATH(0 - readme.js);
 
-request (fpath, function (error, response, body) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(JSON.parse(body));
-	}
+request(fpath, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body));
+  }
 });
